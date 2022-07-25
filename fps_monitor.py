5d5426a490d745ade59d22fe44a8664283bc7b3c
chenhang <chenhang@meizu.com>
2022-07-25 10:29:27 +0800

    add matplotlib

--- fps_monitor.py
+++ fps_monitor.py
@@ -4,7 +4,14 @@ from optparse import OptionParser
 import re
 import subprocess
 import time
-import signal
+import matplotlib.pyplot as plt
+import sys
+import os
+from matplotlib import _pylab_helpers
+
+sample_time = []
+sample_fps = []
+
 
 def query_surfaceflinger_frame_count():
     parcel = subprocess.Popen("adb shell service call SurfaceFlinger 1013",
@@ -14,39 +21,66 @@ def query_surfaceflinger_frame_count():
     if not parcel:
         raise Exception("FAILED: adb shell service call SurfaceFlinger 1013")
 
-    framecount = re.search("Result: Parcel\\(([a-f0-9]+) ", parcel)
+    framecount = re.search("Result: Parcel\\(([a-f0-9]+) ", parcel.decode())
     if not framecount:
-        raise Exception("Unexpected result from SurfaceFlinger: " + parcel)
+        raise Exception("Unexpected result from SurfaceFlinger: " + parcel.decode())
 
     return int(framecount.group(1), 16)
-
-
-def handler(signum, frame):
-    print("handle signal")
 
 
 def main(interval):
     startframe = query_surfaceflinger_frame_count()
     starttime = time.time()
+    begintime = starttime
+
+    plt.ion()
+    fig = plt.figure(figsize=(12, 6))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.grid(True, which='both', linestyle='--')
+
     try:
         while True:
             time.sleep(interval)
+            manager = _pylab_helpers.Gcf.get_active()
+            if manager is None:
+                break
 
             endframe = query_surfaceflinger_frame_count()
             endtime = time.time()
             fps = (endframe - startframe) / (endtime - starttime)
-            print("%.2f" % fps)
-            print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+            sample_time.append(endtime - begintime)
+            sample_fps.append(fps)
+            print("%.3f" % fps)
+
+            ax.set_title('FPS Monitor')
+            ax.set_xlabel('Time')
+            ax.set_ylabel('Frame Per Second')
+            ax.plot(sample_time, sample_fps, 'b-')
+            fig.canvas.draw()
+            plt.pause(0.001)
 
             startframe = endframe
             starttime = endtime
     except:
-        exit()
+        pass
+
+    saveData()
+    plt.close()
+    sys.exit()
+
+
+def saveData():
+    fig = plt.figure(figsize=(12, 6))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_title('FPS Monitor')
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Frame Per Second')
+    ax.plot(sample_time, sample_fps, 'b-')
+    plt.savefig(os.getcwd() + '/savefig.png')
+
 
 
 if __name__ == '__main__':
-    # signal.signal(signal.SIGINT, handler)
-    # signal.signal(signal.SIGTERM, handler)
     parser = OptionParser()
     parser.add_option("-i", "--interval", type="int", default="500", help="Interval of milliseconds to count frames")
     (options, args) = parser.parse_args()
